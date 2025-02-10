6f7ead94bc7134b959a508acfa93e74160a37fd3
keloggzzz <kelis.clark10@gmail.com>
2025-02-10 13:39:56 -0500

    Testing that things function properly

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Welcome to BearcatBoard</h1>
+    <h1>I am just adding things to ensure this is functioning</h1>
     <App />
   </React.StrictMode>
 );
@@ -14,4 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
